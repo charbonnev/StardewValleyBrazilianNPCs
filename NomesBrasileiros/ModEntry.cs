f121c1ea9b7caaba4464e08448b6042eee7a3906
Charles <charles@charbonn.com.br>
2020-05-31 20:15:22 -0300

    Add project files.

---
+++ NomesBrasileiros/ModEntry.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace NomesBrasileiros
+{
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod, IAssetLoader
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            //helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        //private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        //{
+        //    // ignore if player hasn't loaded a save yet
+        //    if (!Context.IsWorldReady)
+        //        return;
+
+        //    // print button presses to the console window
+        //    this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        //}
+
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            //if (asset.AssetNameEquals("Data/NPCDispositions.pt-BR"))
+            if (asset.AssetNameEquals("Data/NPCDispositions"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>Load a matched asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public T Load<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Data/NPCDispositions"))
+            {
+                return this.Helper.Content.Load<T>("assets/NPCDispositions.pt-BR3.xnb", ContentSource.ModFolder);
+            }
+
+            throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
+        }
+    }
+}
